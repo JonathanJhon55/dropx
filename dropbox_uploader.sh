4e8476797f0a6ef69fa9dae0fecb06cd5df5f5c5
JonathanJhon55 <jonathanbogaert5@outlook.be>
2020-02-03 18:55:16 +0000

    Update dropbox_uploader.sh

--- dropbox_uploader.sh
+++ dropbox_uploader.sh
@@ -1548,7 +1548,13 @@ else
     echo -ne " under the 'Generated access token' section, then copy and paste the new access token here:\n\n"
 
     echo -ne " # Access token: "
+    read -r OAUTH_ACCESS_TOKEN
 
+    echo -ne "\n > The access token is $OAUTH_ACCESS_TOKEN. Looks ok? [y/N]: "
+   
+    remove_temp_files
+    exit 0
+fi
 
 ################
 #### START  ####
